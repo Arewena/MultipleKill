ec6cda2de2128dcd2e825d17e4fb46c91c1a0eab
CosyTreq <74582539+Arewena@users.noreply.github.com>
2022-08-18 22:07:16 +0900

    코드 개선

--- src/main/kotlin/io/github/arewena/Main.kt
+++ src/main/kotlin/io/github/arewena/Main.kt
@@ -18,16 +18,27 @@ class Main : JavaPlugin(), Listener {
     private val killPoint = hashMapOf<String, Int>()
     var task: BukkitTask? = null
     var pentaTask: BukkitTask? = null
+
     fun count(player: Player) {
         task = Bukkit.getScheduler().runTaskLater(this, Runnable {
             killPoint[player.name] = 0
         }, 200L)
     }
+
     fun pentaCount(player: Player) {
         pentaTask = Bukkit.getScheduler().runTaskLater(this, Runnable {
             killPoint[player.name] = 0
         }, 400L)
     }
+
+    fun MessagetoAll(player: Player, player2: Player, kill : String, namedTextColor: NamedTextColor, tf : Boolean) {
+        for (i in this.server.onlinePlayers) {
+            i.sendMessage(Component.text("$player(이)가 $player2 을(를) 처치하고 ").append
+                (Component.text(kill).color(namedTextColor).decoration(TextDecoration.BOLD, tf).append
+                (Component.text("킬 달성!"))))
+        }
+    }
+
     override fun onEnable() { this.server.pluginManager.registerEvents(this, this) }
 
 
@@ -52,10 +63,10 @@ class Main : JavaPlugin(), Listener {
 
 
             when (killPoint[event.player.killer!!.name]) {
-                2 -> event.deathMessage(Component.text(event.deathMessage + " 더블 킬!").color(NamedTextColor.YELLOW))
-                3 -> event.deathMessage(Component.text(event.deathMessage + " 트리플 킬!").color(NamedTextColor.YELLOW))
-                4 -> event.deathMessage(Component.text(event.deathMessage + " 쿼드라 킬!").color(NamedTextColor.YELLOW))
-                5 -> event.deathMessage(Component.text(event.deathMessage + " 펜타 킬!").color(NamedTextColor.GOLD).decoration(TextDecoration.BOLD, true))
+                2 -> MessagetoAll(event.player.killer!!, event.player, "더블", NamedTextColor.YELLOW, false)
+                3 -> MessagetoAll(event.player.killer!!, event.player, "트리플", NamedTextColor.YELLOW, false)
+                4 -> MessagetoAll(event.player.killer!!, event.player, "쿼드라", NamedTextColor.YELLOW, false)
+                5 -> MessagetoAll(event.player.killer!!, event.player, "펜타", NamedTextColor.YELLOW, true)
             }
 
             if (killPoint[event.player.killer!!.name]!! < 5) {
